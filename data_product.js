ae73371ebafce29cba4e666c4828346f25597dc0
enioCarloss <eniocarloss@gmail.com>
2022-09-07 21:32:50 -0300

    fiz o possivel para ajustar

---
+++ data_product.js
@@ -0,0 +1,47 @@
+const  paintinsProducts=[
+    { 
+    nome:"Relógio",
+    Preco:"R$ 1200,00",
+    Imagem:"./assets/img/painting/clock.jpg" 
+
+    },
+
+    {
+    nome:"Game Pad",
+    Preco:"R$ 150,00",
+    Imagem:"./assets/img/painting/gamepad.jpg"    
+
+    },
+
+    {
+    nome:"Miniatura C3Po",
+    Preco:"R$ 100,00",
+    Imagem:"./assets/img/painting/pesonagem.jpg"   
+
+    }
+]
+
+const actionsProducts=[
+
+   {
+    nome:"Anime Woman",
+    Preco:"R$ 120,00",
+    Imagem:"./assets/img/actions/animewoman.jpg" 
+
+    },
+
+    {
+    nome:"Miniatura Goku",
+    Preco:"R$ 120,00",
+    Imagem:"./assets/img/actions/dragonballpersonagem.jpg"    
+
+    },
+
+    {
+    nome:"Miniatura Yoda",
+    Preco:"R$ 100,00",
+    Imagem:"./assets/img/actions/starwarspersonagem.jpg"   
+
+    }
+
+]
